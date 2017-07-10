514cbb9c3cf5de34c82f34a753e0141f536b178c
bejf <hbejvl@gmail.com>
2017-07-10 23:27:35 +0200

    init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+// const MongoClient = require('mongodb').MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find().toArray().then((docs) => {
+  //   console.log('Todos')
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }).catch((err) => {
+  //   console.log('Unable to fetch todos'. err);
+  // })
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`)
+  // }).catch((err) => {
+  //   console.log('Unable to fetch todos'. err);
+  // })
+
+  db.collection('Users').find({name: "Honza"}).toArray().then((users) => {
+    console.log(`Users`)
+    console.log(JSON.stringify(users, undefined, 2));
+  }).catch((err) => {
+    console.log('Unable to fetch users'. err);
+  })
+
+  // db.close()
+})
+
+
