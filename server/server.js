bcdc5022973172fef70ff23199ee50ddd4060bf1
bejf <hbejvl@gmail.com>
2017-07-12 23:57:05 +0200

    add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { ObjectID } = require('mongodb')
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo')
 const { User } = require('./models/user')
@@ -26,10 +27,22 @@ app.get('/todos', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e)
   })
-
-
 })
 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send()
+    }
+    res.send({todo})
+  }).catch((e) => res.status(400).send())
+})
 
 app.listen(3000, () => {
   console.log('Server is live on port 3000');
